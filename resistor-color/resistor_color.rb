5050d5b536be2d7f23be6fa2fc326a744846e0b1
Bruno Azuara <bruno.azuara@gmail.com>
2021-06-08 12:11:53 +0200

    add resistor-color

---
+++ resistor-color/resistor_color.rb
@@ -0,0 +1,6 @@
+class ResistorColor
+	COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+	def self.color_code(i)
+		return COLORS.index(i)
+	end
+end
