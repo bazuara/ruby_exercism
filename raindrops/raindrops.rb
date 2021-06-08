289a297191606096f8ac4635f53e87e7e3d13af9
Bruno Azuara <bruno.azuara@gmail.com>
2021-06-08 14:43:57 +0200

    raindrops seccond iteration

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,21 +1,19 @@
 class Raindrops
-	def self.convert(i)
-		ret = ""
-		if i % 3 == 0
-			ret << "Pling"
+	def self.convert(number)
+		drip = ""
+		if number % 3 == 0
+			drip << "Pling"
 		end
-		if i % 5 == 0
-			ret << "Plang"
+		if number % 5 == 0
+			drip << "Plang"
 		end
-		if i % 7 == 0
-			ret << "Plong"
+		if number % 7 == 0
+			drip << "Plong"
 		end
-		if i % 3 != 0 && i % 5 != 0 && i % 7 != 0
-			return i.to_s
+		if drip == nil
+			number.to_s
 		else
-			return ret
+			drip
 		end
 	end
 end
-
-p Raindrops.convert(5)
