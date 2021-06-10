1dbde94d7795809e61f62f69a494ed3c55393e81
Bruno Azuara <bruno.azuara@gmail.com>
2021-06-10 17:25:25 +0200

    raindrops final iteration

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,19 +1,9 @@
 class Raindrops
 	def self.convert(number)
 		drip = ""
-		if number % 3 == 0
-			drip << "Pling"
-		end
-		if number % 5 == 0
-			drip << "Plang"
-		end
-		if number % 7 == 0
-			drip << "Plong"
-		end
-		if drip == nil
-			number.to_s
-		else
-			drip
-		end
+		drip << "Pling"	if (number % 3).zero?
+		drip << "Plang"	if (number % 5).zero?
+		drip << "Plong" if (number % 7).zero?
+		drip.empty? ? number.to_s : drip
 	end
 end
